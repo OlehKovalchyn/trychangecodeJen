b4c606aeb92b47ffa1b3340153825d8449784d38
OlehKovalchyn <49445002+OlehKovalchyn@users.noreply.github.com>
2019-08-20 12:14:42 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -56,7 +56,6 @@ postgresql_server_version_value=${postgresql_server_version_value:-""}
 database_admin_user_value=${database_admin_user_value:-""}
 database_admin_password_value=${database_admin_password_value:-""}
 
-#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 # =======================================================================================================
 # PostgreSQL databases information
 # =======================================================================================================
@@ -82,6 +81,30 @@ database_admin_password_value=${database_admin_password_value:-""}
 FirebaseServerKeyValue=${FirebaseServerKeyValue:-""}
 #VaultTokenValue=
 
+#*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+# =======================================================================================================
+# Kubernetes resourse group & cluster information
+# =======================================================================================================
+kubernetes_resource_group_value=${kubernetes_resource_group_value:-""}
+kubernetes_cluster_name_value=${kubernetes_cluster_name_value:-""}
+# ^ The name of the Azure Kubernetes Service cluster.
+kubernetes_node_count_value=${kubernetes_node_count_value:-""}
+# ^ The number of agents nodes to provision in the cluster
+kubernetes_os_type_value=${kubernetes_os_type_value:-""}
+kubernetes_version_value=${kubernetes_version_value:-""} # Strongly recomended to use this version "1.12.8"
+# ^ https://docs.microsoft.com/en-us/azure/aks/supported-kubernetes-versions#supported-version-list
+kubernetes_vm_size_value=${kubernetes_vm_size_value:-""}
+# ^ "Standard_DS2_v2" (2 vcpus, 7 GiB memory)
+# ^ "Standard_B2ms" (2 vcpus, 8 GiB memory)
+# ^ "Standard_F4s" (4 vcpus, 8 GiB memory)
+# ^ "Standard_DS3_v2" (4 vcpus, 14 GiB memory)
+# kubernetes_client_id_value= automatic from script
+# kubernetes_client_secret_value= automatic from script
+storage_account_name_value=${storage_account_name_value:-""}
+# ^ It must be 3 to 24 characters long, and can contain only lowercase letters and numbers.
+# subscription_id_value= automatic from script
+# tenant_id_value= automatic from script
+
 
 
 
@@ -101,6 +124,10 @@ echo ${FirebaseServerKeyValue}
 
 #*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
 
-
-
-
+echo ${kubernetes_resource_group_value}
+echo ${kubernetes_cluster_name_value}
+echo ${kubernetes_node_count_value}
+echo ${kubernetes_os_type_value}
+echo ${kubernetes_version_value}
+echo ${kubernetes_vm_size_value}
+echo ${storage_account_name_value}
