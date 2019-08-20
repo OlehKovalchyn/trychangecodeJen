caf41a15a7e38f84a56847063438e7555b1872e9
OlehKovalchyn <49445002+OlehKovalchyn@users.noreply.github.com>
2019-08-20 11:52:13 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,11 @@ AZ_LOGIN=${AZ_LOGIN:-"admin@ukr.net"}
 AZ_PASSW=${AZ_PASSW:-""}
 AZ_SUBSCRIPTION_NAME=${AZ_SUBSCRIPTION_NAME:-""}
 AzureQueryResult="AzureQueryResult"
+AzureServicePrincipalResult="AzureServicePrincipalResult"
+azurerm_resource_group_value=${azurerm_resource_group_value:-""}
+
 echo ${AZ_LOGIN}
 echo ${AZ_PASSW}
 echo ${AZ_SUBSCRIPTION_NAME}
 echo ${AzureQueryResult}
+echo ${azurerm_resource_group_value}
